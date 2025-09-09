94cfcc22791c6cdf02c0d40a8105760c5649829a
Sato Kenta <hannirock123@gmail.com>
2025-09-10 01:33:18 +0800

    test string

--- src/t_string.cpp
+++ src/t_string.cpp
@@ -3,6 +3,7 @@
 //
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,6 +16,14 @@ int main() {
     printf("a out of range: %c\n", a[6]);
     printf("new a: %s\n", a.append("G").c_str());
     printf("new a2: %s\n", a.c_str());
+    a.pop_back();
+    printf("pop_back a: %s\n", a.c_str());
+    reverse(a.begin(), a.end());
+    printf("reverse a: %s\n", a.c_str());
+    transform(a.begin(), a.end(), a.begin(), [](unsigned char c){
+      return tolower(c);
+    });
+    printf("tolower a: %s\n", a.c_str());
     a.clear();
     printf("clear a: %s\n", a.c_str());
     return 0;
